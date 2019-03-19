bece148dc976a7b0a7a998c2be3b6326cd7dfa36
Mikha'el <director@mikhaelbd.com>
2019-03-19 14:12:11 -0700

    Updated ProfileScreen

--- react-native-expo/screens/ProfileScreen.js
+++ react-native-expo/screens/ProfileScreen.js
@@ -1,46 +1,86 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, AsyncStorage } from 'react-native';
 import { Container, Content, Card, Header, Title, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from "native-base";
 import { Ionicons, Entypo, MaterialIcons } from '@expo/vector-icons';
-
-
+import ProgressCircle from 'react-native-progress-circle';
+import { API } from "aws-amplify"
 
 class ProfileScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profile: {}
+    }
+  }
+
+  async componentDidMount() {
+    const id = await AsyncStorage.getItem('id')
+    await console.log(id)
+    const profile = await API.get('profile', `/profile/${id}`)
+    await this.setState({ profile: profile[0] })
+    await console.log(this.state.profile)
+    // const percent = this.state.profile
+  }
   render() {
+
+    const progressPercent = parseInt(this.state.profile.xp) / 5000;
+
     return (
       <Container>
         <Content>
-          <Card style={{flex: 0}}>
-            <CardItem>
-            <View style={styles.container}>
+          
+          <Card style={{flex: 1, flexDirection: 'column'}}>
+          <Text>{`\n`}</Text>
+          
 
-            <Thumbnail round large source={{uri: 'https://avatars3.githubusercontent.com/u/15205259?s=400&u=64ad9374b8d98f09dc5709fcc737e5ec4f2447f3&v=4'}} style={{height: 200, width: 200, flex: 1, alignContent: 'center', alignItems: 'center'}} />
-            {/* <Text style={{paddingTop: 10}}><Icon type="FontAwesome" name="map-pin" />Currently in - Seattle</Text> */}
-              <Left>
-              <Text style={{fontSize: 24, fontStyle: "italic", paddingBottom: 10}}>{`\nMichael Litchev`}</Text>
-              </Left>
+            <View style={styles.container}>
+              <Thumbnail round large source={{uri: 'https://avatars3.githubusercontent.com/u/15205259?s=400&u=64ad9374b8d98f09dc5709fcc737e5ec4f2447f3&v=4'}} style={{height: 200, width: '50%', paddingBottom: 40}} />
+                <Right style={{paddingRight: 30}}>
+                <Text style={{fontSize: 20}}>{this.state.profile.name}</Text>
+                <ProgressCircle
+                  percent={progressPercent}
+                  radius={50}
+                  borderWidth={8}
+                  color="#3399FF"
+                  shadowColor="#999"
+                  bgColor="#fff"
+              >
+                  <Text style={{ fontSize: 18 }}>{progressPercent}%</Text>
+              </ProgressCircle>
+              <Text>{parseInt(this.state.profile.xp)}/5000 EXP</Text>
+                </Right>
+              </View>
+
             <CardItem>
-                <Left style={{alignContent: "center"}}>
-                  <Text style={{fontSize: 20}}>Developer{"\n"}<Text note>Technical Rank</Text></Text>
-                </Left>
-                <Right style={{alignContent: "right"}}>
-                  <Text style={{fontSize: 20}}>Organizer{"\n"}<Text note>Community Rank</Text></Text>
+            <Left>
+                <Button transparent textStyle={{color: '#87838B'}} onPress={() => this.props.navigation.navigate('EditProfile', { type: 'EDIT', profile: this.state.profile})}>
+                  <Entypo name="new-message" size={24} />
+                  <Text>Edit Profile</Text>
+                </Button>
+              </Left>  
+                <Right>
+                <Text style={{fontSize: 20}}>{this.state.profile.tRank}{"\n"}<Text note>Technical Rank</Text></Text>
                 </Right>
             </CardItem>
-            <Text style={{textAlign: "center"}}>{`\nI'm a self-taught developer who specializes in React Web & Native development, using Node.js, serverless computing & Amazon Web Services to deliver value to my clients & employers. `}</Text>
-          </View>
-            </CardItem>
+            <Text style={{textAlign: 'center'}}>I'm a self-taught developer who specializes in React Web & Native development, using Node.js, serverless computing & Amazon Web Services to deliver value to my clients & employers.</Text>
+            
             <CardItem>
               <Left>
                 <Button transparent textStyle={{color: '#87838B'}}>
-                  <Entypo name="new-message" size={24} />
-                  <Text>Send Message</Text>
+                  <Entypo name="github" size={24} />
+                  <Text>{this.state.profile.github}</Text>
                 </Button>
               </Left>
+              <Body>
+              <Button transparent textStyle={{color: '#87838B'}}>
+                  <Entypo name="map" size={24} />
+                  <Text>{this.state.profile.region}</Text>
+                </Button>
+              </Body>
               <Right>
               <Button transparent textStyle={{color: '#87838B'}}>
-                  <Entypo name="archive" size={24} />
-                  <Text>View Resume</Text>
+                  {/* <Entypo name="archive" size={24} /> */}
+                  <Text>{this.state.profile.city}, {this.state.profile.country}</Text>
                 </Button>
               </Right>
             </CardItem>
@@ -54,14 +94,15 @@ class ProfileScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    
-    alignContent: 'center',
-    justifyContent: 'center',
-    paddingTop: 20,
-
-    
+    flexDirection: 'row',
+    // alignItems: 'left',
+    // alignContent: 'left',
+    // justifyContent: 'left',
+    // paddingTop: '20', 
   },
+  header: {
+    flexDirection: 'column'
+  }
 });
 
 export default ProfileScreen;
