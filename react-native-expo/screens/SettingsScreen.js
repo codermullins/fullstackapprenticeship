7e24281025c026df0e464e91797dc4bab731bfc8
Mikha'el <director@mikhaelbd.com>
2019-04-05 11:33:07 -0700

    Added Auth dependency for logout, not sure if needed

--- react-native-expo/screens/SettingsScreen.js
+++ react-native-expo/screens/SettingsScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, Button, AsyncStorage } from 'react-native';
+import { Auth } from "aws-amplify"
 
 class SettingsScreen extends Component {
   signOut = async () => {
