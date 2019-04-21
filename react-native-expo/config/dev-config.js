d9b5b5eb0125c9efa2876f84af45bea455b12fed
Mikha'el <director@mikhaelbd.com>
2019-04-21 11:40:54 -0700

    Added dev-config and google services json

---
+++ react-native-expo/config/dev-config.js
@@ -0,0 +1,17 @@
+// Rename this file to config.js
+export default {
+    amplify: {
+        REGION: "string",
+        URL: "string"
+    },
+    sls: {
+        REGION: "string",
+        URL: "string"
+    },
+    cognito: {
+        REGION: "string",
+        USER_POOL_ID: "string",
+        APP_CLIENT_ID: "string",
+        IDENTITY_POOL_ID: "string"
+    }
+};
