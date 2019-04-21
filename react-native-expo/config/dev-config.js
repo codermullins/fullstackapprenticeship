84911136ff51c8dc3f92c6526d7a3887b821475e
Mikha'el <director@mikhaelbd.com>
2019-04-21 12:17:17 -0700

    Smarter dummy data for cognito config

--- react-native-expo/config/dev-config.js
+++ react-native-expo/config/dev-config.js
@@ -1,17 +1,17 @@
 // Rename this file to config.js
 export default {
     amplify: {
-        REGION: "string",
-        URL: "string"
+        REGION: "us-west-2",
+        URL: "https://x5sdfs3.execute-api.us-west-2.amazonaws.com/Prod"
     },
     sls: {
-        REGION: "string",
-        URL: "string"
+        REGION: "us-west-2",
+        URL: "https://r789ncs9.execute-api.us-west-2.amazonaws.com/prod"
     },
     cognito: {
-        REGION: "string",
-        USER_POOL_ID: "string",
-        APP_CLIENT_ID: "string",
-        IDENTITY_POOL_ID: "string"
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_d4BpbyK",
+        APP_CLIENT_ID: "67efmh82vqoodc0q0tgi",
+        IDENTITY_POOL_ID: "us-west-2:9fe677d453-400-a442-f1beeff21731"
     }
 };
