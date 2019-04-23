86e3f8b619b0d510db115d95429ace43147f03da
Mikha'el <director@mikhaelbd.com>
2019-04-23 10:05:11 -0700

    Added two mongo functions to get all mentors and all students for a particular mentor

--- backend/fsa-sls/users.js
+++ backend/fsa-sls/users.js
@@ -75,15 +75,49 @@ module.exports.delete = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   return mongoose.connect(process.env.DB)
     .then(() =>
-      User.findOneAndRemove({ id: event.pathParameters.id })
+      User.findOneAndRemove({ id: event.pathParameters.id})
     )
     .then(user => callback(null, {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Removed user with id: ' + user.id, user: user })
+      body: JSON.stringify({ message: 'Removed user with id: ' + user._id, user: user })
     }))
     .catch(err => callback(null, {
       statusCode: err.statusCode || 500,
       headers: { 'Content-Type': 'text/plain' },
       body: 'Could not fetch the users.'
     }));
+};
+
+module.exports.getAllStudents = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return mongoose.connect(process.env.DB)
+    .then(() =>
+      User.find({ mentor: event.pathParameters.id })
+    )
+    .then(users => callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(users)
+    }))
+    .catch(err => callback(null, {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Could not fetch the users.'
+    }))
+};
+
+module.exports.getAllInstructors = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  return mongoose.connect(process.env.DB)
+    .then(() =>
+      User.find({ instructor: true })
+    )
+    .then(users => callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(users)
+    }))
+    .catch(err => callback(null, {
+      statusCode: err.statusCode || 500,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Could not fetch the users.'
+    }))
 };
