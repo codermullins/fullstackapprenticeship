52b7d6e82d6f036998ea6f6ff43d1d8c4d27c927
Dan Luong <danluong.83@gmail.com>
2019-05-19 13:50:12 -0700

    Added Login Forms

--- react-web/src/Routes.js
+++ react-web/src/Routes.js
@@ -5,21 +5,46 @@ import { history } from "./store/configureStore";
 
 import AppliedRoute from "./components/AppliedRoute";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
-// import Home from "./containers/Home";
+import Home from "./containers/Home";
 import LinkTabs from "./containers/LinkTabs";
 import LinkComponent from "./components/LinkComponent";
 import ToolsContainer from "./components/ToolsContainer";
 import NewResource from "./containers/NewResource";
 import ProcessResource from "./containers/ProcessResource";
+import Signup from "./containers/Signup";
 
 export default ({ childProps }) => (
-    <ConnectedRouter history={history}>
-        <Switch>
-            <AppliedRoute path="/" exact component={LinkTabs}/>
-            <AppliedRoute path="/link/:id" component={LinkComponent} />
-            <AppliedRoute path="/tool/:schema" component={ToolsContainer} />
-            <AppliedRoute path='/review' exact component={ProcessResource} />
-            <AppliedRoute path='/resource/new' exact component={NewResource} props={childProps} /> 
-        </Switch>
-    </ConnectedRouter>
+  <ConnectedRouter history={history}>
+    <Switch>
+      <AppliedRoute path="/" exact component={Home} props={childProps} />
+      <AppliedRoute
+        path="/signup"
+        exact
+        component={Signup}
+        props={childProps}
+      />
+      <AppliedRoute
+        path="/link/:id"
+        component={LinkComponent}
+        props={childProps}
+      />
+      <AppliedRoute
+        path="/tool/:schema"
+        component={ToolsContainer}
+        props={childProps}
+      />
+      <AppliedRoute
+        path="/review"
+        exact
+        component={ProcessResource}
+        props={childProps}
+      />
+      <AppliedRoute
+        path="/resource/new"
+        exact
+        component={NewResource}
+        props={childProps}
+      />
+    </Switch>
+  </ConnectedRouter>
 );
