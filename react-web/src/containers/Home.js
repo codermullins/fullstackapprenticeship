06d6260f4166a65ae48266095af334dc605d1631
Dan <danluong.83@gmail.com>
2019-06-01 18:15:05 -0700

    Create edit profile page and route

--- react-web/src/containers/Home.js
+++ react-web/src/containers/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { API, Auth } from "aws-amplify";
+import {
+  Button,
+  HelpBlock,
+  FormGroup,
+  FormControl,
+  ControlLabel
+} from "react-bootstrap";
 // import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import LinkTabs from "./LinkTabs";
@@ -45,7 +52,8 @@ class Home extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      id: ""
+      id: "",
+      profile: {}
     };
   }
   async componentDidMount() {
@@ -64,9 +72,61 @@ class Home extends React.PureComponent {
 
   async fetchProfile(id) {
     const profile = await API.get("fsa", `/users/${id}`);
-    console.log(id);
-    console.log("Length: ", profile.length);
-    await this.setState({ profile: profile[0] });
+    console.log("id: " + id);
+    console.log("profile: ", profile);
+    this.setState({ profile: profile });
+  }
+
+  renderProfile() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Create Your Profile</h2>
+
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="phone" bsSize="large">
+          <ControlLabel>Phone</ControlLabel>
+          <FormControl
+            autoFocus
+            // type="phone_number"
+            value={this.state.phone}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={this.state.password}
+            onChange={this.handleChange}
+            type="password"
+          />
+        </FormGroup>
+        <FormGroup controlId="confirmPassword" bsSize="large">
+          <ControlLabel>Confirm Password</ControlLabel>
+          <FormControl
+            value={this.state.confirmPassword}
+            onChange={this.handleChange}
+            type="password"
+          />
+        </FormGroup>
+        <Button
+          block
+          bsSize="large"
+          // disabled={!this.validateForm()}
+          type="submit"
+          // isLoading={this.state.isLoading}
+          text="Signup"
+          // loadingText="Signing up…"
+        />
+      </form>
+    );
   }
 
   //   Based on the isAuthenticated state object, we can render either the LinkTabs or the SignUp
@@ -76,8 +136,8 @@ class Home extends React.PureComponent {
       <MuiThemeProvider theme={theme}>
         <div className={classes.background}>
           {this.props.isAuthenticated ? (
-            <React.Fragment>{this.props.id}</React.Fragment>
-          ) : (
+            this.renderProfile()
+            ) : (
             <Login userHasAuthenticated={this.props.userHasAuthenticated} />
           )}
         </div>
