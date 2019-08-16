04b3437cef55cd0186a77cd0c91f72142d7fb33c
Michael Litchev <mikhael@Michaels-MacBook-Pro.local>
2019-08-16 11:04:46 -0700

    Added config.js test data for people clone

---
+++ react-native-expo/config/config.js
@@ -0,0 +1,16 @@
+export default {
+    amplify: {
+        REGION: "us-west-2",
+        URL: "https://x5d788qn76.execute-api.us-west-2.amazonaws.com/Prod"
+    },
+    sls: {
+        REGION: "us-west-2",
+        URL: "https://rfx4b7d1s9.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_d4BELL8OK",
+        APP_CLIENT_ID: "67efmh82veodju1sjfp80q0tgi",
+        IDENTITY_POOL_ID: "us-west-2:9fe677d7-a799-4e10-a442-f1beeff21731"
+    }
+};
