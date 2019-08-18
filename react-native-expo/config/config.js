59418a1b057d08a0658fe8721e22c9987be7f775
george <geomarray@gmail.com>
2019-08-18 13:49:21 -0700

    added withAuthenticator and removed existing auth components

--- react-native-expo/config/config.js
+++ react-native-expo/config/config.js
@@ -1,16 +1,20 @@
 export default {
-    amplify: {
-        REGION: "us-west-2",
-        URL: "https://x5d788qn76.execute-api.us-west-2.amazonaws.com/Prod"
+    // amplify: {
+    //     REGION: "us-west-2",
+    //     URL: "https://x5d788qn76.execute-api.us-west-2.amazonaws.com/Prod"
+    // },
+    // mongo: {
+    //     REGION: "us-west-2",
+    //     URL: "https://rfx4b7d1s9.execute-api.us-west-2.amazonaws.com/prod"
+    // },
+    // cognito: {
+    //     REGION: "us-west-2",
+    //     USER_POOL_ID: "us-west-2_d4BELL8OK",
+    //     APP_CLIENT_ID: "67efmh82veodju1sjfp80q0tgi",
+    //     IDENTITY_POOL_ID: "us-west-2:9fe677d7-a799-4e10-a442-f1beeff21731"
+    // }
+    mongo: {
+        REGION: "us-east-1",
+        URL: "https://t80wm4bvrd.execute-api.us-east-1.amazonaws.com/prod"
     },
-    sls: {
-        REGION: "us-west-2",
-        URL: "https://rfx4b7d1s9.execute-api.us-west-2.amazonaws.com/prod"
-    },
-    cognito: {
-        REGION: "us-west-2",
-        USER_POOL_ID: "us-west-2_d4BELL8OK",
-        APP_CLIENT_ID: "67efmh82veodju1sjfp80q0tgi",
-        IDENTITY_POOL_ID: "us-west-2:9fe677d7-a799-4e10-a442-f1beeff21731"
-    }
 };
--- react-native-expo/screens/SignUpScreen.js
+++ react-native-expo/screens/SignUpScreen.js
@@ -22,7 +22,7 @@ class SignUpScreen extends Component {
 
   register = async () => {
     const attributes = {
-      email: this.state.email, 
+      email: this.state.email,
       phone_number: this.state.phone
     }
 
@@ -32,43 +32,44 @@ class SignUpScreen extends Component {
         username: this.state.email,
         attributes: attributes,
       });
-      
+
       this.setState({ confirm: true })
-    } catch(e) {
+    } catch (e) {
       console.log('Auth error: ', e)
     }
 
-    
+
   };
 
   confirm = async () => {
     try {
-      await Auth.confirmSignUp(this.state.email, this.state.code); 
+      const signUp = await Auth.confirmSignUp(this.state.email, this.state.code);
+      console.log('signup ====>', sign)
       this.props.navigation.navigate('SignIn');
-    } catch(e) {
+    } catch (e) {
       console.log('SignUp error: ', e)
     }
   };
 
   renderConfirmation() {
-    return(
+    return (
       <Container>
-         <Content>
+        <Content>
           <Form>
             <Item floatingLabel>
               <Label>code</Label>
               <Input
-              placeholder="Type Here"
-              returnKeyType="search"
-              value={this.state.code}
-              onChangeText={(code) => this.setState({ code })} 
-              autoCapitalize="none"
+                placeholder="Type Here"
+                returnKeyType="search"
+                value={this.state.code}
+                onChangeText={(code) => this.setState({ code })}
+                autoCapitalize="none"
               />
             </Item>
             <Text>{'\n'}</Text>
-            <Text style={{textAlign: 'center', fontSize: 14}}>Please check your email for a 6 digit confirmation code.</Text>
+            <Text style={{ textAlign: 'center', fontSize: 14 }}>Please check your email for a 6 digit confirmation code.</Text>
             <Text>{'\n'}</Text>
-        <Button block style={{backgroundColor: "#6200EE", color: "white"}} onPress={() => this.confirm()}><Text style={{color: "white"}}>Complete Registration</Text></Button>
+            <Button block style={{ backgroundColor: "#6200EE", color: "white" }} onPress={() => this.confirm()}><Text style={{ color: "white" }}>Complete Registration</Text></Button>
           </Form>
         </Content>
       </Container>
@@ -76,57 +77,57 @@ class SignUpScreen extends Component {
   }
 
   renderRegister() {
-    return(
-    <Container>
-    <Content>
+    return (
+      <Container>
+        <Content>
           <Form>
             <Item floatingLabel>
               <Label>Primary Email</Label>
               <Input
-              placeholder=""
-              returnKeyType="search"
-              value={this.state.email}
-              onChangeText={(email) => this.setState({ email })} 
-              autoCapitalize="none"
+                placeholder=""
+                returnKeyType="search"
+                value={this.state.email}
+                onChangeText={(email) => this.setState({ email })}
+                autoCapitalize="none"
               />
             </Item>
             <Item floatingLabel>
               <Label>Phone</Label>
-              <Input 
-              placeholder=""
-              returnKeyType="search"
-              value={this.state.phone}
-              onChangeText={(phone) => this.setState({phone})}
-              autoCapitalize="none"
+              <Input
+                placeholder=""
+                returnKeyType="search"
+                value={this.state.phone}
+                onChangeText={(phone) => this.setState({ phone })}
+                autoCapitalize="none"
               />
             </Item>
             <Item floatingLabel last>
               <Label>Password</Label>
-              <Input 
-              placeholder=""
-              returnKeyType="search"
-              value={this.state.password}
-              onChangeText={(password) => this.setState({ password})}
-              autoCapitalize="none"
-              secureTextEntry={true}
+              <Input
+                placeholder=""
+                returnKeyType="search"
+                value={this.state.password}
+                onChangeText={(password) => this.setState({ password })}
+                autoCapitalize="none"
+                secureTextEntry={true}
 
               />
             </Item>
             <Text>{'\n'}</Text>
-            <Text style={{textAlign: 'center', fontSize: 14}}>Password must include at least 1 capital letter, 1 lowercase letter, 1 number and be 8 characters long.</Text>
+            <Text style={{ textAlign: 'center', fontSize: 14 }}>Password must include at least 1 capital letter, 1 lowercase letter, 1 number and be 8 characters long.</Text>
             <Text>{'\n'}</Text>
             {
               this.state.email.length > 0 && this.state.password.length > 7 ?
-              (
-                <Button full style={{backgroundColor: "#6200EE", color: "white", height: 80}} onPress={() => this.register()}><Text style={{color: "white", fontSize: 20}}>Sign Up</Text></Button>
+                (
+                  <Button full style={{ backgroundColor: "#6200EE", color: "white", height: 80 }} onPress={() => this.register()}><Text style={{ color: "white", fontSize: 20 }}>Sign Up</Text></Button>
                 ) : (
-                <Button full disabled><Text style={{color: "white"}}>Sign Up</Text></Button>
+                  <Button full disabled><Text style={{ color: "white" }}>Sign Up</Text></Button>
 
-              )
+                )
             }
           </Form>
         </Content>
-    </Container>
+      </Container>
     )
   }
 
@@ -134,9 +135,9 @@ class SignUpScreen extends Component {
     return (
       <Container>
         {this.state.confirm === false ?
-        this.renderRegister() : this.renderConfirmation()  
-      }
-      </Container> 
+          this.renderRegister() : this.renderConfirmation()
+        }
+      </Container>
     );
   }
 }
